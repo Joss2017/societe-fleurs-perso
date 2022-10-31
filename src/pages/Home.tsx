53930c0746cbcec7ed4f943c77f9bd5873bb8f8f
Josselin BAPTISTE <josselin.baptiste@sncf.fr>
2022-10-31 18:24:24 +0100

    1.2 Affichage liste-plantes DATA

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { list_products } from "../data";
+
+export interface TextElementPlant {
+  id: string;
+  name: string;
+  unitprice_ati: number;
+  quantity: number;
+  category: string;
+  rating: number;
+  url_picture: string;
+}
+
+const listPlant: TextElementPlant[] = list_products;
 
 const Home = () => {
-  return <div>Page d'accueil</div>;
+  return (
+    <div>
+      <div>
+        {" "}
+        <h1>Page d'accueil</h1>
+      </div>
+      ;
+      <div>
+        {listPlant.map((plante) => (
+          <li>{plante.name}</li>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
