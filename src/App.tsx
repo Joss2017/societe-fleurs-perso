74f215b59951e4ab3bb6f4a0c902af5a11da366c
Josselin BAPTISTE <josselin.baptiste@sncf.fr>
2022-10-31 10:45:18 +0100

    1.0 - Install Bootstrap -Intégration navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
-function App() {
+const App = () => {
+  const message: string = "Hello world";
+
+  const handleClick = (name: string) => alert(`Bonjour ${name} !`);
+  const handleClickSansParam = () => alert("Yes Papa !");
   return (
-    <div className="App">
-      <h1>hello World</h1>
+    <div>
+      {/* On utilise notre composant dans notre JSX */}
+      <NavBar />
+      <p className="color-pink">{message}</p>
+      {/* Liaison de l'évènement click avec l'appel de la fonction handleClick avec paramètre */}
+      <button className="btn btn-success" onClick={() => handleClick("Joseph")}>
+        Test Joseph
+      </button>
+      <button onClick={() => handleClick("COCO")}>Test Coco</button>
+      <p>CouCou</p>
+      {/* Liaison de l'évènement click avec l'appel de la fonction handleClickSansParam sans paramètre */}
+      <button onClick={handleClickSansParam}>TEST 2</button>
     </div>
   );
-}
+};
 
 export default App;
